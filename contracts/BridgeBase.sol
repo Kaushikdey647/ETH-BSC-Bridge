23834221da0eb3efc31d48919ec6f1b10b6f7dfa
Kaushik Dey <Kaushikdey647@gmail.com>
2022-02-08 22:53:12 +0530

    Create Bridge Contracts

--- contracts/BridgeBase.sol
+++ contracts/BridgeBase.sol
@@ -25,10 +25,18 @@ contract BridgeBase {
         // nonce = 0;
         // processed = {};
     }
-
+    //Anyone can call the burn function
     function burn(address to, uint amount) external {
         token.burn(msg.sender, amount);
         emit Transfer(msg.sender, to, amount, block.timestamp, nonce, Step.Burn);
         nonce++;
+    }
+    //Only Admin can call the mint: Bridge API
+    function mint(address to, uint amount, uint otherChainNonce) external {
+        require(msg.sender == admin,'Only admin can mint');
+        require(processed[otherChainNonce] == false, 'Transaction already processed');
+        processed[otherChainNonce] == true; //make processed
+        token.mint(to, amount); //mint on the other side
+        emit Transfer(msg.sender, to, amount, block.timestamp, otherChainNonce, Step.Mint);
     }
 }
